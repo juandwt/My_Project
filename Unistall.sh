ac391f34a6fbf38d9b1cd3cdc79af255de0d7f99
Juan Diego Wilches Torres <134608182+juandwt@users.noreply.github.com>
2024-11-24 12:13:28 -0500

    Update Unistall.sh

--- Unistall.sh
+++ Unistall.sh
@@ -4,29 +4,31 @@
 #       Desinstalador de My Project
 # =======================================
 
+# Confirmar si desea desinstalar
 zenity --question --title="Desinstalación de My Project" --width=400 --height=200 --text="¿Deseas desinstalar My Project?" --ok-label="Sí" --cancel-label="No"
-
 if [ $? -eq 0 ]; then
-    # Eliminar el acceso directo de la aplicación
-    if [ -f ~/.local/share/applications/My_project.desktop ]; then
-        rm ~/.local/share/applications/My_project.desktop
-        zenity --info --width=400 --height=200 --text="El acceso directo de My Project ha sido eliminado."
-    else
-        zenity --warning --width=400 --height=200 --text="El acceso directo no existe o ya ha sido eliminado previamente."
+    # Mostrar ventana de confirmación para eliminar dependencias
+    zenity --question --title="Eliminar Dependencias" --width=400 --height=200 --text="Se eliminarán todas las dependencias relacionadas con My Project. ¿Deseas continuar?" --ok-label="Sí" --cancel-label="No"
+    if [ $? -eq 0 ]; then
+        # Eliminar dependencias
+        SYSTEM_PACKAGES=("git" "python3" "python3-tk" "python3-matplotlib" "python3-numpy" "python3-sympy")
+        for package in "${SYSTEM_PACKAGES[@]}"; do
+            if dpkg -l | grep -q "^ii  $package "; then
+                sudo apt remove --purge -y "$package"
+            fi
+        done
     fi
 
-    # Actualizar la base de datos de aplicaciones
-    zenity --info --width=400 --height=200 --text="Actualizando la base de datos de aplicaciones..."
-    update-desktop-database ~/.local/share/applications/
+    # Eliminar acceso directo del escritorio
+    DESKTOP_DIR=$(xdg-user-dir DESKTOP)
+    if [ -f "$DESKTOP_DIR/My_project.desktop" ]; then
+        rm "$DESKTOP_DIR/My_project.desktop"
+    fi
 
     # Buscar y eliminar la carpeta del proyecto
-    project_path=$(find / -type d -name "My_Project" 2>/dev/null | zenity --list --title="Selecciona la carpeta a eliminar" --text="Carpeta My_Project encontrada en las siguientes ubicaciones:" --column="Rutas")
-
+    project_path=$(find / -type d -name "My_Project" 2>/dev/null)
     if [ -n "$project_path" ]; then
         rm -rf "$project_path"
-        zenity --info --width=400 --height=200 --text="La carpeta My_Project en $project_path ha sido eliminada."
-    else
-        zenity --warning --width=400 --height=200 --text="No se encontró ninguna carpeta llamada My_Project o se canceló la selección."
     fi
 
     # Finalización
