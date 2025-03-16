ceb5649d67645682db13c713be0afb3e068f940d
Juan Diego Wilches Torres <134608182+juandwt@users.noreply.github.com>
2025-03-16 12:21:45 -0500

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,52 +1,73 @@
+###########################################################################
+#                                                                         #
+# NOTICE OF COPYRIGHT                                                     #
+# Educational software aimed at teaching the variational method in        #
+# confined quantum systems.                                                #
+# SEQVS                                                                   #
+#                                                                         #
+# Copyright (C) 2025                                                      #
+#                                                                         #
+# Authors:								                                  #
+#   [1] Julián Salamanca*				                        		  #
+#   [2] Diego Julián Rodríguez-Patarroyo**		                		  #
+#   [3] Juan Diego Wilches Torres***                        			  #
+#                                                                         #
+#   [1] jasalamanca@udistrital.edu.co (profesor Universidad Distrital)    #
+#   [2] jdwilchest@udistrital.edu.co  (Licenciado en física)              #
+#   [3] maramirezramos@utep.edu	       			                          #
+#									                                      #
+#  * Grupo de Física e Informática (FISINFOR)		     		          #
+#  ** Grupo de Laboratorio de Fuentes Alternas de Energía (LIFAE)	      #
+#  *,** Universidad Distrital Francisco José de Caldas (Bogotá, Colombia) #	
+#   *** University of Texas at El Paso (UTEP) 	  			              #
+#       (USA)                                                             #
+# Web page:								                                  #
+#   https://github.com/fisinforgh/QVS                                      #
+#                                                                         #
+# This program is free software; you can redistribute it and/or modify    #
+# it under the terms of the GNU General Public License as published by    #
+# the Free Software Foundation; either version 2 of the License, or       #
+# (at your option) any later version.                                     #
+#                                                                         #
+# This program is distributed in the hope that it will be useful,         #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+# GNU General Public License for more details:                            #
+#                                                                         #
+#               http://www.gnu.org/copyleft/gpl.html                      #
+#                                                                         #
+###########################################################################
+
 #!/bin/bash -i
 
-# 1. Descargar Install.sh 
-
-# *. sudo apt update for last version of Ubuntu
-
-# 2. chmod +x Install.sh  
-# 3. Revisar dependencias, entre ellas git para descargar el proyecto
-# 4. Descargar proyecto con git clone (preguntar dónde quiere guardar el proyecto)
-# 5. Crear icono (.desktop) en /home/$USER/.local/share/applications/
-# 6. chmod +x .desktop
-# 7. Finalizar instalación
-
-# Rutas necesarias
 DESKTOP_DIR=$(xdg-user-dir DESKTOP)
 
-# Mensaje de bienvenida
 zenity --info --title="Bienvenido al instalador" --width=400 --height=200 --text="Vamos a verificar las dependencias" --ok-label="Continuar"
 
-# Lista de paquetes necesarios
 SYSTEM_PACKAGES=("git" "python3" "python3-tk" "python3-matplotlib" "python3-numpy" "python3-scipy")
 
-# Función para verificar si los paquetes están instalados y preparar comandos de instalación
 check_packages_status() {
     package_status=()
-    package_installed=() # Arreglo booleano para marcar 1 si está instalado, 0 si no
+    package_installed=() 
     all_installed=true  
 
     for package in "${SYSTEM_PACKAGES[@]}"; do
         if apt list --installed 2>/dev/null | grep -q "^$package/"; then
             package_status+=("✔" "$package" "Instalado" "sudo apt install -y $package")
-            package_installed+=(1) # Agrega 1 al arreglo
+            package_installed+=(1) 
         else
             package_status+=("✘" "$package" "No instalado" "sudo apt install -y $package")
-            package_installed+=(0) # Agrega 0 al arreglo
+            package_installed+=(0)
             all_installed=false
         fi
     done
 
-    # Mostrar el estado de los paquetes y comandos de instalación en una tabla
     zenity --list --title="Verificación de Dependencias" \
         --text="Estado de los paquetes necesarios:" \
         --width=750 --height=410 \
         --column="Estado" --column="Paquete" --column="Disponibilidad" --column="Comando de instalación" \
         "${package_status[@]}"
 
-    echo "Arreglo booleano de dependencias instaladas: ${package_installed[*]}"
-
-    # Instalar dependencias faltantes automáticamente después de mostrar la tabla
     if [ "$all_installed" = false ]; then
         for package in "${SYSTEM_PACKAGES[@]}"; do
             if ! apt list --installed 2>/dev/null | grep -q "^$package/"; then
@@ -66,11 +87,8 @@ check_packages_status() {
     fi
 }
 
-# Llama a la función para verificar e instalar dependencias
 check_packages_status
 
-
-# Pregunta al usuario dónde desea clonar el proyecto
 PROJECT_DIR=$(zenity --file-selection --directory --title="Selecciona el directorio donde deseas guardar el proyecto")
 
 if [ -z "$PROJECT_DIR" ]; then
@@ -78,7 +96,6 @@ if [ -z "$PROJECT_DIR" ]; then
     exit 1
 fi
 
-# Clonar el proyecto de GitHub
 git clone https://github.com/juandwt/My_Project.git "$PROJECT_DIR/My_Project"
 
 if [ $? -ne 0 ]; then
@@ -86,7 +103,6 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Crear el archivo .desktop para el acceso directo
 echo "[Desktop Entry]
 Version=1.0
 Name=QVS
@@ -96,11 +112,9 @@ Type=Application
 Terminal=false
 Categories=Development;" > "$DESKTOP_DIR/My_project.desktop"
 
-# Otorgar permisos de ejecución al archivo .desktop
 chmod +x "$DESKTOP_DIR/My_project.desktop"
 gio set "$DESKTOP_DIR/My_project.desktop" metadata::trusted true
 
 zenity --info --width=500 --height=200 --text="El acceso directo ha sido creado en el Escritorio" --ok-label="Continuar"
 
-# Finalizar instalación
 zenity --info --width=500 --height=200 --text="¡Gracias por instalar My Project!" --ok-label="Finalizar"
