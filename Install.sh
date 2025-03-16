60e76c8b72386dff9890a891efc89b4e64584fef
Juan Diego Wilches Torres <134608182+juandwt@users.noreply.github.com>
2025-03-16 12:22:58 -0500

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -8,9 +8,9 @@
 # Copyright (C) 2025                                                          #
 #                                                                             #
 # Authors:                                                                    #
-#   [1] Julián Salamanca*                            	                    #
+#   [1] Julián Salamanca*                            	                      #
 #   [2] Diego Julián Rodríguez-Patarroyo**                                    #
-#   [3] Juan Diego Wilches Torres***                                           #
+#   [3] Juan Diego Wilches Torres***                                          #
 #                                                                             #
 #   [1] jasalamanca@udistrital.edu.co (profesor Universidad Distrital)        #
 #   [2] jdwilchest@udistrital.edu.co  (Licenciado en física)                  #
@@ -27,7 +27,7 @@
 # This program is free software; you can redistribute it and/or modify        #
 # it under the terms of the GNU General Public License as published by        #
 # the Free Software Foundation; either version 2 of the License, or           #
-# (at your option) any later version.                                          #
+# (at your option) any later version.                                         #
 #                                                                             #
 # This program is distributed in the hope that it will be useful,             #
 # but WITHOUT ANY WARRANTY; without even the implied warranty of              #
